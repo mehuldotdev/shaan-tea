7eefc619a7acae126716eabb2eeaf828dbf0009b
mehuldotdev <mehehehul748@gmail.com>
2025-08-29 20:33:50 +0530

    ts + nextjs

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import { LogOut, Moon, Menu, Ghost, GhostIcon, Coffee } from 'lucide-react'
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <div className='flex flex-col justify-between bg-black w-full h-16'>
+    </div>
+  )
+}
+
+export default Header
